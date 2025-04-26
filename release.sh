ed30b5aac826cf0e0791d3476fe7286ae3322640
epicmo <epicmocn@gmail.com>
2025-04-26 21:39:55 +0800

    init

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 now=$(date +"%Y%m%d%H%M%S")
-docker buildx build --platform linux/amd64 -t epicmo/soruxgpt-saas-audit:latest --push .
-docker buildx build --platform linux/amd64 -t epicmo/soruxgpt-saas-audit:latest --push .
+docker buildx build --platform linux/amd64 -t epicmo/soruxgpt-saas-audit-oauth:latest --push .
+docker buildx build --platform linux/amd64 -t epicmo/soruxgpt-saas-audit-oauth:latest --push .
 
 echo "release success" $now
